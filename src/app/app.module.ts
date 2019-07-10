57c520009e0a34e5fd3cb8b6ee93a8783fcef97d
Sebastian Schiefermayr <basti@bastiarts.com>
2019-07-10 23:06:26 +0200

    Added fancy particles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,14 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {MaterialModule} from './modules/material/material.module';
 import {AppRoutingModule} from './app-routing.module';
+import {ParticlesModule} from 'angular-particle';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 
 import {AppComponent} from './app.component';
 import {ClientAreaComponent} from './components/client-area/client-area.component';
 import {LoginComponent} from './components/login/login.component';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { RegisterComponent } from './components/register/register.component';
-import { DashboardComponent } from './components/client-area/dashboard/dashboard.component';
+import {RegisterComponent} from './components/register/register.component';
+import {DashboardComponent} from './components/client-area/dashboard/dashboard.component';
 import {AuthGuard} from './services/auth.guard';
 
 @NgModule({
@@ -25,7 +26,8 @@ import {AuthGuard} from './services/auth.guard';
         AppRoutingModule,
         FormsModule,
         MaterialModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
+        ParticlesModule
     ],
     providers: [AuthGuard],
     bootstrap: [AppComponent]
--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -1,11 +1,81 @@
-import { Injectable } from '@angular/core';
+import {Injectable} from '@angular/core';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class DataService {
-  public darkmode = false;
+    public darkmode = false;
+
+    public particles: object = {
+        style: {
+            'position': 'fixed',
+            'width': '100%',
+            'height': '100%',
+            'z-index': 1,
+            'top': 0,
+            'left': 0,
+            'right': 0,
+            'bottom': 0
+        },
+        params: {
+            particles: {
+                number: {
+                    value: 100,
+                },
+                color: {
+                    value: '#ff0000'
+                },
+                shape: {
+                    type: 'circle'
+                },
+                line_linked: {
+                    enable: false
+                },
+                interactivity: {
+                    detect_on: 'canvas',
+                    events: {
+                        onhover: {
+                            enable: false,
+                            mode: 'grab'
+                        },
+                        onclick: {
+                            enable: false,
+                            mode: 'push'
+                        },
+                        resize: true
+                    },
+                    modes: {
+                        grab: {
+                            distance: 200,
+                            line_linked: {
+                                opacity: 1
+                            }
+                        },
+                        bubble: {
+                            distance: 200,
+                            size: 80,
+                            duration: 0.4
+                        },
+                        repulse: {
+                            distance: 200,
+                            duration: 0.4
+                        },
+                        push: {
+                            particles_nb: 4
+                        },
+                        remove: {
+                            particles_nb: 2
+                        }
+                    },
+                    mouse: {}
+                }
+            }
+        },
+        width: 100,
+        height: 100
+    };
 
 
-  constructor() { }
+    constructor() {
+    }
 }
