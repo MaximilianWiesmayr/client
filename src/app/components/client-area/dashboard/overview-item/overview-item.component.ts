05f02271ca99ae86933d0a5c13cdb2af7a328c61
Sebastian Schiefermayr <basti@bastiarts.com>
2019-08-02 18:40:40 +0200

    More improvements according responsiveness Added the dynamic Overview-Items

---
+++ src/app/components/client-area/dashboard/overview-item/overview-item.component.ts
@@ -0,0 +1,17 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+    // tslint:disable-next-line:component-selector
+    selector: 'overview-item',
+    templateUrl: './overview-item.component.html',
+    styleUrls: ['./overview-item.component.scss']
+})
+export class OverviewItemComponent implements OnInit {
+
+    constructor() {
+    }
+
+    ngOnInit() {
+    }
+
+}
