fae439d6bc6139c10ebc3b9df54a52d0ecb636ae
Sebastian Schiefermayr <basti@bastiarts.com>
2019-08-05 11:14:02 +0200

    Added List View Mode in 'My Photos' Section

--- src/app/entities/Settings.ts
+++ src/app/entities/Settings.ts
@@ -1,7 +1,8 @@
 export class Settings {
     constructor(
         public darkmode: boolean = false,
-        public navBarCollapsed: boolean = false
+        public navBarCollapsed: boolean = false,
+        public myPhotosGridView: boolean = true
     ) {
     }
 }
