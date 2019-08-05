caf0637454563205c4688a250b9be9cf18328dfe
Sebastian Schiefermayr <basti@bastiarts.com>
2019-08-05 11:37:09 +0200

    Small Layout-Fixes

--- src/app/entities/Image.ts
+++ src/app/entities/Image.ts
@@ -5,7 +5,8 @@ export class Image {
         public title: string = '', // custom title set by the user, by default its the factory name
         public factoryTitle: string = '', // DSG00151 bla bla
         public path: string = '', // Storage Path
-        public extension: ImageExtension = ImageExtension.JPG // ImageExtension
+        public extension: ImageExtension = ImageExtension.JPG, // ImageExtension
+        public fileSize: string = '0MB'
 
     ) {
     }
